49738088141fca2b8d51e476737ef16ab682677a
i3Cheese <i3Cheese@yandex.ru>
2020-04-21 22:36:06 +0700

    errors handlers

--- app/web_pages.py
+++ app/web_pages.py
@@ -224,3 +224,36 @@ def league_console(league_id: int):
         abort(403)
 
     return render_template("league_console.html", league=league)
+
+
+@blueprint.errorhandler(400)
+def bad_request(error):
+    message="Что-то пошло не так. Наш сервис не может обработать такой запрос."
+    return render_template("errors/error.html", 
+                           error=error, 
+                           message=message)
+    
+    
+@blueprint.errorhandler(401)
+def unauthorized(error):
+    message="Для этого действия требуется авторизация."
+    return render_template("errors/error401.html", 
+                           error=error, 
+                           message=message)
+    
+
+@blueprint.errorhandler(403)
+def forbidden(error):
+    message="Вам запрещено делать то, что вы пытаетесь. Ничего не поделаешь, такие правила."
+    return render_template("errors/error.html", 
+                           error=error, 
+                           message=message)
+    
+
+@blueprint.errorhandler(404)
+def not_found(error):
+    return render_template("errors/error.html", 
+                           error=error, 
+                           message="Мы не нашли то что вы искали. Или вы искали то чего у нас нет.")
+    
+
