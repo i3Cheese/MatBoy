44f4b7699515b850decad9893fa614a6faf9ac88
Daxa <apelsinKDA@gmail.com>
2020-06-04 00:36:20 +0700

    turn on/off vk notification

--- bot/launch.py
+++ bot/launch.py
@@ -8,4 +8,4 @@ USERS_INFO = {}
 def bot_launch():
     for event in LONGPOLL.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
-            handler(event.obj.message['from_id'], USERS_INFO)
+            handler(event.obj.message['from_id'], event.obj.message['text'], USERS_INFO)
--- bot/utils.py
+++ bot/utils.py
@@ -2,8 +2,43 @@ from data import User, create_session
 import bot.messages as messages
 
 
-def handler(uid, users_info):
+def handler(uid, text, users_info):
     if uid not in users_info.keys():
         users_info[uid] = {}
         messages.welcome_message(uid)
         return
+    else:
+        if text.lower() not in ('выключить', 'включить'):
+            messages.auto_answer(uid)
+        else:
+            notifications(uid, text.lower())
+
+
+def notifications(uid, command):
+    session = create_session()
+    user = session.query(User).filter(User.vk_id == uid).first()
+    if not user:
+        messages.send_message(uid, 'К сожалению Ваша страница ' \
+                                   'ВКонтакте не привязана к аккаунту на сайте')
+        return
+    if command == 'включить':
+        turn_on_notification(uid, user)
+    elif command == 'выключить':
+        turn_off_notification(uid, user)
+    session.commit()
+
+
+def turn_on_notification(uid, user):
+    if user.vk_notifications:
+        messages.send_message(uid, 'У Вас уже включены уведомления')
+    else:
+        user.vk_notifications = True
+        messages.send_message(uid, 'Уведомления успешно включены')
+
+
+def turn_off_notification(uid, user):
+    if not user.vk_notifications:
+        messages.send_message(uid, 'У Вас отключены уведомления')
+    else:
+        user.vk_notifications = False
+        messages.send_message(uid, 'Уведомления успешно отключены')
